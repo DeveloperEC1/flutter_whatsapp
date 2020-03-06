49adb50c7abbebf28238de81f34b24b920b53a6f
EliorCohen <elior123456123123@gmail.com>
2020-03-06 15:17:05 +0200

    Initial update

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter WhatsApp',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.white,
       ),
       home: ListPage(title: 'Flutter WhatsApp'),
     );
